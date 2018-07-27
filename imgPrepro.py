2718a168db6de1b8e1a5f762338e4e601489623a
lmyXcom <33785596+lmyXcom@users.noreply.github.com>
2018-07-27 09:35:12 +0900

    Update imgPrepro.py

--- imgPrepro.py
+++ imgPrepro.py
@@ -1,11 +1,13 @@
 # reference: https://github.com/yunjey/show-attend-and-tell
 
-# use Python 2.7
-# but I cannot use tensorflow in Python 2.7 on WINDOWS...
+# use Python 2.7 -> modified to Python 3.x on July 27
+# (but I cannot use tensorflow in Python 2.7 on WINDOWS...(
 # "TensorFlow supports Python 3.5.x and 3.6.x on Windows."
 
 # Before training the model, you have to preprocess the MSCOCO caption dataset.
 # To generate caption dataset and image feature vectors, run command below.
+
+# Python3 버젼의 prepro.py
 
 from scipy import ndimage
 from collections import Counter
@@ -24,7 +26,7 @@ def _process_caption_data(caption_file, image_dir, max_length):
     with open(caption_file) as f:
         caption_data = json.load(f)
 
-    # id_to_filename is a dictionary such as {image_id: filename]}
+    # id_to_filename is a dictionary such as {image_id: filename]} 
     id_to_filename = {image['id']: image['file_name'] for image in caption_data['images']}
 
     # data is a list of dictionary which contains 'captions', 'file_name' and 'image_id' as key.
@@ -33,28 +35,28 @@ def _process_caption_data(caption_file, image_dir, max_length):
         image_id = annotation['image_id']
         annotation['file_name'] = os.path.join(image_dir, id_to_filename[image_id])
         data += [annotation]
-
+    
     # convert to pandas dataframe (for later visualization or debugging)
     caption_data = pd.DataFrame.from_dict(data)
     del caption_data['id']
     caption_data.sort_values(by='image_id', inplace=True)
     caption_data = caption_data.reset_index(drop=True)
-
+    
     del_idx = []
     for i, caption in enumerate(caption_data['caption']):
-        caption = caption.replace('.', '').replace(',', '').replace("'", "").replace('"', '')
-        caption = caption.replace('&', 'and').replace('(', '').replace(")", "").replace('-', ' ')
+        caption = caption.replace('.','').replace(',','').replace("'","").replace('"','')
+        caption = caption.replace('&','and').replace('(','').replace(")","").replace('-',' ')
         caption = " ".join(caption.split())  # replace multiple spaces
-
+        
         caption_data.set_value(i, 'caption', caption.lower())
         if len(caption.split(" ")) > max_length:
             del_idx.append(i)
-
+    
     # delete captions if size is larger than max_length
-    print "The number of captions before deletion: %d" % len(caption_data)
+    print("The number of captions before deletion: %d" %len(caption_data))
     caption_data = caption_data.drop(caption_data.index[del_idx])
     caption_data = caption_data.reset_index(drop=True)
-    print "The number of captions after deletion: %d" % len(caption_data)
+    print("The number of captions after deletion: %d" %len(caption_data))
     return caption_data
 
 
@@ -62,45 +64,45 @@ def _build_vocab(annotations, threshold=1):
     counter = Counter()
     max_len = 0
     for i, caption in enumerate(annotations['caption']):
-        words = caption.split(' ')  # caption contrains only lower-case words
+        words = caption.split(' ') # caption contrains only lower-case words
         for w in words:
-            counter[w] += 1
-
+            counter[w] +=1
+        
         if len(caption.split(" ")) > max_len:
             max_len = len(caption.split(" "))
 
     vocab = [word for word in counter if counter[word] >= threshold]
-    print ('Filtered %d words to %d words with word count threshold %d.' % (len(counter), len(vocab), threshold))
+    print(('Filtered %d words to %d words with word count threshold %d.' % (len(counter), len(vocab), threshold)))
 
-    word_to_idx = {u'<NULL>': 0, u'<START>': 1, u'<END>': 2}
+    word_to_idx = {'<NULL>': 0, '<START>': 1, '<END>': 2}
     idx = 3
     for word in vocab:
         word_to_idx[word] = idx
         idx += 1
-    print "Max length of caption: ", max_len
+    print("Max length of caption: ", max_len)
     return word_to_idx
 
 
 def _build_caption_vector(annotations, word_to_idx, max_length=15):
     n_examples = len(annotations)
-    captions = np.ndarray((n_examples, max_length + 2)).astype(np.int32)
+    captions = np.ndarray((n_examples,max_length+2)).astype(np.int32)   
 
     for i, caption in enumerate(annotations['caption']):
-        words = caption.split(" ")  # caption contrains only lower-case words
+        words = caption.split(" ") # caption contrains only lower-case words
         cap_vec = []
         cap_vec.append(word_to_idx['<START>'])
         for word in words:
             if word in word_to_idx:
                 cap_vec.append(word_to_idx[word])
         cap_vec.append(word_to_idx['<END>'])
-
+        
         # pad short caption with the special null token '<NULL>' to make it fixed-size vector
         if len(cap_vec) < (max_length + 2):
             for j in range(max_length + 2 - len(cap_vec)):
-                cap_vec.append(word_to_idx['<NULL>'])
-
+                cap_vec.append(word_to_idx['<NULL>']) 
+        
         captions[i, :] = np.asarray(cap_vec)
-    print "Finished building caption vectors"
+    print("Finished building caption vectors")
     return captions
 
 
@@ -131,11 +133,11 @@ def _build_image_idxs(annotations, id_to_idx):
 def main():
     # batch size for extracting feature vectors from vggnet.
     batch_size = 100
-    # maximum length of caption(number of word). if caption is longer than max_length, deleted.
+    # maximum length of caption(number of word). if caption is longer than max_length, deleted.  
     max_length = 15
     # if word occurs less than word_count_threshold in training dataset, the word index is special unknown token.
     word_count_threshold = 1
-    # vgg model path
+    # vgg model path 
     vgg_model_path = './data/imagenet-vgg-verydeep-19.mat'
 
     caption_file = 'data/annotations/captions_train2014.json'
@@ -154,7 +156,7 @@ def main():
     # about 4000 images and 20000 captions for val / test dataset
     val_cutoff = int(0.1 * len(val_dataset))
     test_cutoff = int(0.2 * len(val_dataset))
-    print 'Finished processing caption data'
+    print('Finished processing caption data')
 
     save_pickle(train_dataset, 'data/train/train.annotations.pkl')
     save_pickle(val_dataset[:val_cutoff], 'data/val/val.annotations.pkl')
@@ -166,7 +168,7 @@ def main():
         if split == 'train':
             word_to_idx = _build_vocab(annotations=annotations, threshold=word_count_threshold)
             save_pickle(word_to_idx, './data/%s/word_to_idx.pkl' % split)
-
+        
         captions = _build_caption_vector(annotations=annotations, word_to_idx=word_to_idx, max_length=max_length)
         save_pickle(captions, './data/%s/%s.captions.pkl' % (split, split))
 
@@ -187,7 +189,7 @@ def main():
                 feature_to_captions[i] = []
             feature_to_captions[i].append(caption.lower() + ' .')
         save_pickle(feature_to_captions, './data/%s/%s.references.pkl' % (split, split))
-        print "Finished building %s caption dataset" % split
+        print("Finished building %s caption dataset" %split)
 
     # extract conv5_3 feature vectors
     vggnet = Vgg19(vgg_model_path)
@@ -203,18 +205,18 @@ def main():
 
             all_feats = np.ndarray([n_examples, 196, 512], dtype=np.float32)
 
-            for start, end in zip(range(0, n_examples, batch_size),
-                                  range(batch_size, n_examples + batch_size, batch_size)):
+            for start, end in zip(list(range(0, n_examples, batch_size)),
+                                  list(range(batch_size, n_examples + batch_size, batch_size))):
                 image_batch_file = image_path[start:end]
-                image_batch = np.array(map(lambda x: ndimage.imread(x, mode='RGB'), image_batch_file)).astype(
+                image_batch = np.array([ndimage.imread(x, mode='RGB') for x in image_batch_file]).astype(
                     np.float32)
                 feats = sess.run(vggnet.features, feed_dict={vggnet.images: image_batch})
                 all_feats[start:end, :] = feats
-                print ("Processed %d %s features.." % (end, split))
+                print(("Processed %d %s features.." % (end, split)))
 
             # use hickle to save huge feature vectors
             hickle.dump(all_feats, save_path)
-            print ("Saved %s.." % (save_path))
+            print(("Saved %s.." % (save_path)))
 
 
 if __name__ == "__main__":
