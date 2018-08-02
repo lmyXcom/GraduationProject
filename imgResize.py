e9cde1e0811572c4539cc8f329875149a0253a85
lmyXcom <33785596+lmyXcom@users.noreply.github.com>
2018-08-02 11:43:26 +0900

    Update imgResize.py

--- imgResize.py
+++ imgResize.py
@@ -4,32 +4,30 @@
 from PIL import Image
 import os
 
-
 def resize_image(image):
     width, height = image.size
     if width > height:
-        left = (width - height) / 2
-        right = width - left
+        left = (width-height)/2
+        right = width-left
         top = 0
         bottom = height
     else:
-        top = (height - width) / 2
-        bottom = height - top
+        top = (height-width)/2
+        bottom = height-top
         left = 0
         right = width
     image = image.crop((left, top, right, bottom))
-    image = image.resize([224, 224], Image.ANTIALIAS) # modify if I want another size
+    image = image.resize([224, 224], Image.ANTIALIAS)
     return image
-
 
 def main():
     splits = ['train', 'val']
     for split in splits:
-        folder = 'C:/Users/my_name/Downloads/sample_flickr30k/%s' %split
-        resized_folder = 'C:/Users/my_name/Downloads/sample_flickr30k/%s_resized' %split
+        folder = '/home/most12lee/downloads/data/%s' %split
+        resized_folder = '/home/most12lee/downloads/data/%s_resized' %split
         if not os.path.exists(resized_folder):
             os.makedirs(resized_folder)
-        print 'Start resizing %s images.' % split
+        print 'Start resizing %s images.' %split
         image_files = os.listdir(folder)
         num_images = len(image_files)
         for i, image_file in enumerate(image_files):
@@ -38,8 +36,8 @@ def main():
                     image = resize_image(image)
                     image.save(os.path.join(resized_folder, image_file), image.format)
             if i % 100 == 0:
-                print 'Resized images: %d/%d' % (i, num_images)
-
-
+                print 'Resized images: %d/%d' %(i, num_images)
+              
+            
 if __name__ == '__main__':
     main()
