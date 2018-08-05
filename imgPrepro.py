5ea2c7ddeb8b15b848246c45d767066653a53d0b
lmyXcom <33785596+lmyXcom@users.noreply.github.com>
2018-08-05 16:16:21 +0900

    Update imgPrepro.py

--- imgPrepro.py
+++ imgPrepro.py
@@ -1,13 +1,6 @@
-# reference: https://github.com/yunjey/show-attend-and-tell
-
-# use Python 2.7 -> modified to Python 3.x on July 27
-# (but I cannot use tensorflow in Python 2.7 on WINDOWS...(
-# "TensorFlow supports Python 3.5.x and 3.6.x on Windows."
-
+# from: https://github.com/yunjey/show-attend-and-tell
 # Before training the model, you have to preprocess the MSCOCO caption dataset.
 # To generate caption dataset and image feature vectors, run command below.
-
-# Python3 버젼의 prepro.py
 
 from scipy import ndimage
 from collections import Counter
@@ -53,10 +46,10 @@ def _process_caption_data(caption_file, image_dir, max_length):
             del_idx.append(i)
     
     # delete captions if size is larger than max_length
-    print("The number of captions before deletion: %d" %len(caption_data))
+    print "The number of captions before deletion: %d" %len(caption_data)
     caption_data = caption_data.drop(caption_data.index[del_idx])
     caption_data = caption_data.reset_index(drop=True)
-    print("The number of captions after deletion: %d" %len(caption_data))
+    print "The number of captions after deletion: %d" %len(caption_data)
     return caption_data
 
 
@@ -72,14 +65,14 @@ def _build_vocab(annotations, threshold=1):
             max_len = len(caption.split(" "))
 
     vocab = [word for word in counter if counter[word] >= threshold]
-    print(('Filtered %d words to %d words with word count threshold %d.' % (len(counter), len(vocab), threshold)))
+    print ('Filtered %d words to %d words with word count threshold %d.' % (len(counter), len(vocab), threshold))
 
-    word_to_idx = {'<NULL>': 0, '<START>': 1, '<END>': 2}
+    word_to_idx = {u'<NULL>': 0, u'<START>': 1, u'<END>': 2}
     idx = 3
     for word in vocab:
         word_to_idx[word] = idx
         idx += 1
-    print("Max length of caption: ", max_len)
+    print "Max length of caption: ", max_len
     return word_to_idx
 
 
@@ -102,7 +95,7 @@ def _build_caption_vector(annotations, word_to_idx, max_length=15):
                 cap_vec.append(word_to_idx['<NULL>']) 
         
         captions[i, :] = np.asarray(cap_vec)
-    print("Finished building caption vectors")
+    print "Finished building caption vectors"
     return captions
 
 
@@ -156,7 +149,7 @@ def main():
     # about 4000 images and 20000 captions for val / test dataset
     val_cutoff = int(0.1 * len(val_dataset))
     test_cutoff = int(0.2 * len(val_dataset))
-    print('Finished processing caption data')
+    print 'Finished processing caption data'
 
     save_pickle(train_dataset, 'data/train/train.annotations.pkl')
     save_pickle(val_dataset[:val_cutoff], 'data/val/val.annotations.pkl')
@@ -189,7 +182,7 @@ def main():
                 feature_to_captions[i] = []
             feature_to_captions[i].append(caption.lower() + ' .')
         save_pickle(feature_to_captions, './data/%s/%s.references.pkl' % (split, split))
-        print("Finished building %s caption dataset" %split)
+        print "Finished building %s caption dataset" %split
 
     # extract conv5_3 feature vectors
     vggnet = Vgg19(vgg_model_path)
@@ -205,18 +198,18 @@ def main():
 
             all_feats = np.ndarray([n_examples, 196, 512], dtype=np.float32)
 
-            for start, end in zip(list(range(0, n_examples, batch_size)),
-                                  list(range(batch_size, n_examples + batch_size, batch_size))):
+            for start, end in zip(range(0, n_examples, batch_size),
+                                  range(batch_size, n_examples + batch_size, batch_size)):
                 image_batch_file = image_path[start:end]
-                image_batch = np.array([ndimage.imread(x, mode='RGB') for x in image_batch_file]).astype(
+                image_batch = np.array(map(lambda x: ndimage.imread(x, mode='RGB'), image_batch_file)).astype(
                     np.float32)
                 feats = sess.run(vggnet.features, feed_dict={vggnet.images: image_batch})
                 all_feats[start:end, :] = feats
-                print(("Processed %d %s features.." % (end, split)))
+                print ("Processed %d %s features.." % (end, split))
 
             # use hickle to save huge feature vectors
             hickle.dump(all_feats, save_path)
-            print(("Saved %s.." % (save_path)))
+            print ("Saved %s.." % (save_path))
 
 
 if __name__ == "__main__":
